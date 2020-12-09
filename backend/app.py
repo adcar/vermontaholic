2e710c2ba80f8a8ff06c746b14a893910c284b20
Silas <48316528+silas730@users.noreply.github.com>
2020-12-09 16:21:18 -0500

    Added more endpoints

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect, jsonify
 from flask_login import login_required, current_user, login_user, logout_user
-from userdb import Users, db, login
+from userdb import Users, db, login, Locations
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -11,6 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db.init_app(app)
 login.init_app(app)
 login.login_view = 'login'
+
 
 
 # Creates the database if one does not already exist
@@ -38,7 +39,7 @@ def register():
         # Checks if the username already exists
         if Users.query.filter_by(username=username).first():
             return jsonify({'status': 'error',
-                            'msg': 'Username already taken'})  # this should be changed to a notification on the page
+                            'msg': 'Username already taken'}) 
 
         user = Users(username=username)
         user.set_password(password)
@@ -62,7 +63,7 @@ def login():
             return jsonify({'status': 'success', 'msg': 'Successfully logged in', 'username': username})
         else:
             return jsonify(
-                {'status': 'error', 'msg': 'Failed to login'})  # this should be changed to a notification on the page
+                {'status': 'error', 'msg': 'Failed to login'})  
     else:
         return jsonify({'status': 'error', 'msg': 'This endpoint only accepts POST requests'})
 
@@ -71,5 +72,34 @@ def login():
 def logout():
     logout_user()
     return jsonify({'status': 'success', 'msg': 'Successfully logged out'})
+
+@app.route('/towns/<user>', methods=['GET'])
+def getVisitedTowns(user):
+    rows = Locations.query.all()
+
+    locationList = []
+    for row in rows:
+        if user in row.visitors:
+            isVisited = True
+        else:
+            isVisited = False
+        
+        locationList.append({'name' : row.location, 'isVisited' : isVisited})
+    return jsonify(locationList)
+
+@app.route('/<user>/visit/<visitedTown>')
+#@login_required #disabled for testing purposes
+def visitTown(user, visitedTown):
+    town = Locations.query.filter_by(location = visitedTown).first()
+    if town == None:
+        return jsonify({'status': 'failure', 'msg': 'Town not Found'})
+
+    visitorString = town.visitors
+    visitorString = visitorString + ',' + user
+    town.visitors = visitorString
+    db.session.add(town)
+    db.session.commit()
+    
+    return jsonify({'status': 'success', 'msg': 'Successfully visited town'})
 
 # app.run(host='localhost', port=5000)
--- backend/userdb.py
+++ backend/userdb.py
@@ -11,13 +11,20 @@ class Users(UserMixin, db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
-    hashed_pass = db.Column(db.String())
+    hashed_pass = db.Column(db.String(64))
 
     def set_password(self,password):
         self.hashed_pass = bcrypt.generate_password_hash(password).decode('utf-8')
 
     def check_password(self,password):
         return bcrypt.check_password_hash(self.hashed_pass, password)
+
+class Locations(db.Model):
+    __tablename__ = 'locations'
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String(64))
+    visitors = db.Column(db.String(255))
+
 
 @login.user_loader
 def load_user(id):
